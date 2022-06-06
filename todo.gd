72988d1e527a38f97f6b34a7043af871402bef37
Simon Boigelot <sboigelot@gmail.com>
2022-06-06 19:28:36 +0200

    Input visual in tutorial

--- todo.gd
+++ todo.gd
@@ -3,8 +3,6 @@
 # add score?
 # add "You reached level x in x time" in victory screen
 
-# trigger: button (can be activated by orb / arrow / hammer)
-
 # split main menu & join game
 # setting change volume music / soundfx
 # implement sound fx
